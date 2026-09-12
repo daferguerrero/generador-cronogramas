9a8a14dfe62c59a3a20a93383f1ace94eef959db
daferguerrero <dafer_guerrero@hotmail.com>
2026-09-12 17:08:21 -0500

    feat(HU-02): Implementar la configuración de horarios y las reglas de validación

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 
 import planningRoutes from "./routes/planning.routes.js";
+import scheduleConfigRoutes from "./routes/schedule-config.routes.js";
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/planning', planningRoutes)
+app.use('/planning', planningRoutes);
+app.use('/schedule-config', scheduleConfigRoutes);
 
 export default app;
